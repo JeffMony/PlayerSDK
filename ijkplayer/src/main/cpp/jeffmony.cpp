f834fd08e78103210607af1129238a6b3c090161
JeffMony <jeffmony@163.com>
2021-05-25 16:59:05 +0800

    去掉ijkplayer中不用的逻辑
    Signed-off-by: JeffMony <jeffmony@163.com>

--- ijkplayer/src/main/cpp/jeffmony.cpp
+++
@@ -1,9 +0,0 @@
-#include <jni.h>
-#include <string>
-
-extern "C"
-JNIEXPORT int JNICALL
-Java_com_jeffmony_video_process_FFmpegRemux_remux(JNIEnv *env, jclass clazz, jstring input_path,
-                                                   jstring output_path) {
-    return 0;
-}
